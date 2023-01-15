b557bc08d1029b6d3bba844b582dc785c01264e1
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-01-14 22:14:20 -0500

    Time to overhaul this mess

--- version_send.lua
+++ version_send.lua
@@ -1,5 +1,10 @@
 local minetest,name = minetest,minetest.localplayer:get_name()
 local version_channel = minetest.mod_channel_join(name..":client_version_channel")
+
+local current_development_cycle = "alpha"
+local crafter_version = 0.071
+
+
 minetest.after(2,function() -- this needs a few seconds for the mod channel to open up
     version_channel:send_all("0.07000")
 end)
